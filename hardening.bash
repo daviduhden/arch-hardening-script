8cd151429d39fcaaa9ab1d3ec2dacbd1b04ddb58
David Uhden <david@uhden.dev>
2025-02-01 13:10:37 +0100

    Minor fixes to the indentation

--- hardening.bash
+++ hardening.bash
@@ -1,21 +1,21 @@
 #!/bin/bash -e
 
-#    Hardening Script for Arch Linux
-#    Copyright (C) 2019 madaidan
-#    Copyright (C) 2025 David Uhden Collado
+# Hardening Script for Arch Linux
+# Copyright (C) 2019 madaidan
+# Copyright (C) 2025 David Uhden Collado
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 while test $# -gt 0; do
   case "$1" in
@@ -458,16 +458,16 @@ EOF
       # Enables systemd service.
       systemctl enable macspoof.service
     elif [ "${which_mac_spoofer}" = "NetworkManager" ]; then
-        # Installs networkmanager if it isn't already installed.
-        if ! pacman -Qq networkmanager &>/dev/null; then
-          read -r -p "NetworkManager is not installed. Install it now? (y/n) " install_networkmanager
-          if [ "${install_networkmanager}" = "y" ]; then
-            pacman -S --noconfirm -q networkmanager
-          fi
+      # Installs networkmanager if it isn't already installed.
+      if ! pacman -Qq networkmanager &>/dev/null; then
+        read -r -p "NetworkManager is not installed. Install it now? (y/n) " install_networkmanager
+        if [ "${install_networkmanager}" = "y" ]; then
+          pacman -S --noconfirm -q networkmanager
         fi
+      fi
 
-        # Randomize MAC address with networkmanager.
-        cat <<EOF > /etc/NetworkManager/conf.d/rand_mac.conf
+      # Randomize MAC address with networkmanager.
+      cat <<EOF > /etc/NetworkManager/conf.d/rand_mac.conf
 [connection-mac-randomization]
 ethernet.cloned-mac-address=random
 wifi.cloned-mac-address=random
